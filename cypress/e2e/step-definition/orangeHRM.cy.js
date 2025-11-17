c44f571a75ff34d7cf3f5c636d596c74eae509ff
Mayur Nawale <mayurnawale9765@gmail.com>
2025-11-17 16:45:15 +0530

    fix bugs

--- cypress/e2e/step-definition/orangeHRM.cy.js
+++ cypress/e2e/step-definition/orangeHRM.cy.js
@@ -5,7 +5,7 @@ import data from'../../fixtures/orangeHRM.json'
 describe('pom',()=>{
 it.only('login test',()=>{
 cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-Login.setUserName(data.ausername);
+Login.setUserName(data.username);
 Login.setPassword(data.password)
 Login.clickSubmit();
 Login.verifyLogin();
